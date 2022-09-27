d8c02d25bc730f0fffb0313e56ea165a89b62096
Luferat <contato@luferat.net>
2022-09-27 11:05:25 -0300

    Testes

--- script.js
+++ script.js
@@ -5,8 +5,8 @@
  * OBS1: Este é o aplicativo principal, para que o tema (template) do site
  * opere. Posteriormente, quando necessário, cada página (conteúdo) terá seu
  * próprio JavaScript, assim, somente o JavaScript necessário será carregado.
- * 
- * OBS1: Todas as funções que iniciam com um cifrão ($) fazem parte da 
+ *
+ * OBS1: Todas as funções que iniciam com um cifrão ($) fazem parte da
  * biblioteca jQuery, ou seja, não são do JavaScript "puro" (vanilla).
  *
  * Para saber mais:
@@ -25,7 +25,7 @@ function runApp() {
   loadPage('home');
 
   /**
-   * jQuery → Quando houver click em um elemento <a>, execute o aplicativo 
+   * jQuery → Quando houver click em um elemento <a>, execute o aplicativo
    * "routerLink":
    **/
   $('a').click(routerLink);
@@ -39,7 +39,7 @@ function routerLink() {
 
   /**
    * jQuery → Recebe o atributo "href" do link clicado e armazena em 'href':
-   * A função "$(this)" faz referência ao elemento que foi clicado e disparou 
+   * A função "$(this)" faz referência ao elemento que foi clicado e disparou
    * este processo.
    **/
   var href = $(this).attr('href');
@@ -47,10 +47,10 @@ function routerLink() {
   /**
    * Se faz referência a link externo que começa com "http://" OU "https://",
    * ou faz referência a uma âncora que começa com "#"...
-   * 
+   *
    * OBS: O código "||" (pipe pipe) significa o "OU" (OR) lógico em JavaScript.
-   * 
-   * Referências: 
+   *
+   * Referências:
    *    https://youtu.be/mp3g9IQ651g
    *    https://www.w3schools.com/jsref/jsref_substring.asp
    *    https://www.w3schools.com/js/js_if_else.asp
@@ -67,14 +67,14 @@ function routerLink() {
   ) {
 
     /**
-     * Encerra o programa retornando "true" (verdade) para que o HTML abra o 
+     * Encerra o programa retornando "true" (verdade) para que o HTML abra o
      * link normalmente:
      **/
     return true;
   }
 
   /**
-   * Se clicou em um link interno (rota para uma página), executa o programa 
+   * Se clicou em um link interno (rota para uma página), executa o programa
    * "loadPage()" que carrega a página correta:
    **/
   loadPage(href);
@@ -87,13 +87,29 @@ function routerLink() {
 }
 
 /**
- * loadPage() → Aplicativo que processa os arquivos HTML, CSS e JavaScript da 
+ * loadPage() → Aplicativo que processa os arquivos HTML, CSS e JavaScript da
  * "rota" solicitada e abre estes no site:
  */
 function loadPage(href) {
 
-  console.log(href);
+  var page = {
+    "html": `/pages/${href}/index.html`,
+    "css": `/pages/${href}/style.css`,
+    "js": `/pages/${href}/script.js`
+  }
 
+  $.get(page.html)
+    .done(function (content) {
+      $('#pageCSS').attr('href', page.css);
+      $('#content').html(content);
+      $.getScript(page.js);
+    });
+
+}
+
+function setTitle(title = '') {
+  if (title == '') $('title').html(`${siteName} .:. ${siteSlogan}`);
+  else $('title').html(`${siteName} .:. ${pageTitle}`);
 }
 
 // jQuery → Executa aplicativo "runApp" quando o documento estiver pronto:
